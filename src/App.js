00caf9983e92844827b27607171949677c5c0acd
Jazmine Foxworth <jazminefoxworth@jazmines-mbp.lan>
2024-06-19 01:12:04 -0500

    project submission
    Committer: bananacostume <bananacostume93@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import avatar from "./assets/avatar-jessica.jpeg";
 
 function App() {
+  const buttonNames = [
+    "Github",
+    "Frontend Mentor",
+    "LinkedIn",
+    "Twitter",
+    "Instagram",
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="social-card">
+        <div className="social-title-card">
+          <img src={avatar} className="social-avatar" alt="jessica-randall" />
+          <h2 className="social-title-name">Jessica Randall</h2>
+          <small className="social-location">London, United Kingdom</small>
+        </div>
+        <small className="social-blurb">
+          "Front-end developer and avid reader."
+        </small>
+        <div className="social-button-group">
+          {buttonNames.map((buttonName, idx) => (
+            <button key={idx}>{buttonName}</button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
